5f2fbc27acf91b84492eba7c3df27325acc1cee0
pangmf <41246258+pangmf@users.noreply.github.com>
2023-03-21 19:52:02 +0800

    Update 555ys.js

--- gx/555ys.js
+++ gx/555ys.js
@@ -3,6 +3,7 @@
 555影视去广告
 */
 [rewrite_local] 
-^https:\/\/vip7\.fzwdyy\.cn:8083\/api\/getAdvertInfo url reject
+^https:\/\/vip7\.fzwdyy\.cn:8083\/api\/getAdvertInfo url reject-200
+^https:\/\/vip7\.fzwdyy\.cn:8083\/api\/getGOOGAdvert url reject-200
 [mitm]
 hostname = vip7.fzwdyy.cn
